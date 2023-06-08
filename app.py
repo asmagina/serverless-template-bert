d536a63160e589a19cab7de429333262b3c550f0
Anna <a.smagina1995@gmail.com>
2023-06-08 19:23:18 +0200

    trying to fix timig on server

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def init():
     device = 0 if torch.cuda.is_available() else -1
     model = BertModel.from_pretrained('bert-base-uncased',          output_hidden_states = True)
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = pipeline("feature-extraction", model=model, tokenizer=tokenizer, device=device)
 
 
 # Inference is ran for every server call
@@ -20,15 +21,15 @@ def inference(model_inputs:dict) -> dict:
 
     # Parse out your arguments
 
-    tokenized_dict = tokenizer.encode_plus(
-        model_inputs.get("text"),
-        add_special_tokens=True,
-        max_length=5
-    )
+    #tokenized_dict = tokenizer.encode_plus(
+    text = model_inputs.get("text"),
+    #    add_special_tokens=True,
+    #    max_length=5
+    #)
 
-    tokenized_text = torch.tensor(tokenized_dict["input_ids"])
+    # tokenized_text = torch.tensor(tokenized_dict["input_ids"])
     with torch.no_grad():
-        embeddings = model(torch.tensor(tokenized_text.unsqueeze(0)))
+        embeddings = model(text)
 
     # Return the results as a dictionary
-    return {'answer': embeddings[0][:, 0, :].squeeze().numpy().tolist()}
+    return {'answer': embeddings}
