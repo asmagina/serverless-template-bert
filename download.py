adbf1452536bf8c5987ef880df848c2405c65a63
Anna <a.smagina1995@gmail.com>
2023-06-08 14:08:25 +0200

    Fix to previous commit

--- download.py
+++ download.py
@@ -1,13 +1,12 @@
 # This file runs during container build time to get model weights built into the container
 
 # In this example: A Huggingface BERT model
-from transformers import pipeline, BertForQuestionAnswering, BertTokenizer
+from transformers import BertModel, BertTokenizer 
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    m = BertForQuestionAnswering.from_pretrained('bert-base-uncased',          output_hidden_states = True,)
+    m = BertModel.from_pretrained('bert-base-uncased')
     t = BertTokenizer.from_pretrained('bert-base-uncased')
-    pipeline('qfeature-extraction', model=m, tokenizer=t)
 
 if __name__ == "__main__":
     download_model()
